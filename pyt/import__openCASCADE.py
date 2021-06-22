018a7147ae4c9bf445c8743b4e49788a795bac73
wfw-pgr <wfw.pgr@gmail.com>
2021-06-22 14:01:45 +0900

    first commit

---
+++ pyt/import__openCASCADE.py
@@ -0,0 +1,75 @@
+import numpy as np
+import os, sys
+import gmsh
+
+
+# ========================================================= #
+# ===  make__geometry                                   === #
+# ========================================================= #
+
+def make__geometry():
+
+    scale   = 1.0e-6
+    inpFile = "model/model.step"
+    
+    gmsh.option.setNumber( "Geometry.OCCScaling", 1.0e-6 )
+    gmsh.model.occ.importShapes( inpFile )
+    
+    return()
+
+    
+
+
+# ========================================================= #
+# ===   実行部                                          === #
+# ========================================================= #
+
+if ( __name__=="__main__" ):
+    
+
+    # ------------------------------------------------- #
+    # --- [1] initialization of the gmsh            --- #
+    # ------------------------------------------------- #
+    gmsh.initialize()
+    gmsh.option.setNumber( "General.Terminal", 1 )
+    gmsh.option.setNumber( "Mesh.Algorithm"  , 1 )
+    gmsh.option.setNumber( "Mesh.Algorithm3D", 1 )
+    gmsh.option.setNumber( "Mesh.SubdivisionAlgorithm", 1 )
+    gmsh.model.add( "model" )
+    
+    
+    # ------------------------------------------------- #
+    # --- [2] Modeling                              --- #
+    # ------------------------------------------------- #
+
+    make__geometry()
+    
+    gmsh.model.occ.synchronize()
+    gmsh.model.occ.removeAllDuplicates()
+    gmsh.model.occ.synchronize()
+
+
+    # ------------------------------------------------- #
+    # --- [3] Mesh settings                         --- #
+    # ------------------------------------------------- #
+    
+    # meshFile = "dat/mesh.conf"
+    # physFile = "dat/phys.conf"
+    # import nkGmshRoutines.assign__meshsize as ams
+    # meshes = ams.assign__meshsize( meshFile=meshFile, physFile=physFile )
+    
+    gmsh.option.setNumber( "Mesh.CharacteristicLengthMin", 0.1 )
+    gmsh.option.setNumber( "Mesh.CharacteristicLengthMax", 0.1 )
+    
+
+    # ------------------------------------------------- #
+    # --- [4] post process                          --- #
+    # ------------------------------------------------- #
+    gmsh.model.occ.synchronize()
+    gmsh.model.mesh.generate(3)
+    gmsh.write( "msh/model.msh" )
+    gmsh.finalize()
+    
+
+
+
